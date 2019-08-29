caf96a0453d99c330e0b396a86dbcc6ca535fa90
Steve Cote <sdcote@gmail.com>
2019-08-29 19:17:32 -0400

    Simple integration test

--- src/test/java/coyote/metrics/prom/PushGatewayClientTest.java
+++ src/test/java/coyote/metrics/prom/PushGatewayClientTest.java
@@ -1,32 +1,29 @@
 package coyote.metrics.prom;
 
+import coyote.metrics.ScoreCard;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import java.io.StringWriter;
-import java.io.Writer;
+import java.io.IOException;
 
 class PushGatewayClientTest {
 
   @Test
-  void push() {
-    Writer writer = new StringWriter();
-
-    System.out.println(writer.toString());
-  }
-
-  @Test
-  void standardUseCase() {
+  @Tag("integration")
+  void standardUseCase() throws IOException {
 
     // create some monitors
+    ScoreCard.startTimer("timer1").setDescription("Test time in milliseconds");
+    ScoreCard.incrementCounter("counter1");
+    ScoreCard.incrementGauge("gauge1");
+    ScoreCard.increaseCounter("counter2", 22);
+    ScoreCard.increaseCounter("counter2", 20);
+    ScoreCard.increaseGauge("gauge2", 52);
+    ScoreCard.decreaseGauge("gauge2", 10);
+    ScoreCard.stopTimer("timer1");
 
-    // at the end of the job, send all of the collected metrics to the push gateway
-    // client.push(monitors, jobName)
-    // client.push(monitors, jobName, instanceName) overriding the instance name from that in the scorecard
-    // each monitor will
-    //   - have its name based on the name of the monitor
-    //   - have its description used as the help text
-    //   - have each of the labels represented
-
+    // push them to the gateway using the defaults (http://localhost:9091)
+    new PushGatewayClient().push("tmetrixTest");
   }
 
 
@@ -37,7 +34,6 @@ class PushGatewayClientTest {
   void testingUseCase() {
     // create monitors with the following labels:
     // - metric_name - the name of the metric to use (the monitor name will be used as the job name instead)
-    // - job_name
 
     // At the end of the job call the client to collect and send metrics based on the internally set metric name:
     // client.pushJobNamedMetrics() - the entire scoreboard
